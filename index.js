93f863a205f9c6906348c29eaf2b92c83921e791
carlos martin <cjmar87@gmail.com>
2018-09-17 16:59:32 -0500

    avanzamos en el api

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/hola/:name',(req, res)=>{
+    res.send({message: `hola ${req.params.name}`})
+})
+
+app.listen(port, () => {
+    console.log(`api rest corriendo en localhost:${port}`)
+})
