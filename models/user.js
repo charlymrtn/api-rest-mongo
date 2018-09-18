9a0b8508627a35bfda00c3f05b8696b291db8cf2
carlos martin <cjmar87@gmail.com>
2018-09-18 16:51:13 -0500

    modelo usuario

---
+++ models/user.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto')
+
+const userSchema = new Schema({
+    email: { type: String, required: true, unique: true, lowercase: true},
+    name: { type: String, required: true},
+    avatar: { type: String, required: true },
+    password: { type: String, required: true, select: false},
+    signupData: { type: Date, default: Date.now()},
+    lastLogin: { type: Date}
+});
+
+userSchema.pre('save', (next)=>{
+    let user = this
+    if(!user.isModified('password')) return next()
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, null, (err,hash)=>{
+            if(err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    })
+})
+
+userSchema.methods.gravatar = function (){
+    if(!this.email) return `https://gravatar.com/avatar/?s=200&d=retro`
+
+    const md5 = crypto.createHash('md5').update(this.email).digest('hex')
+    return `https://gravatar.com/avatar/${md5}?s=200&d=retro`
+
+}
+
+module.exports = mongoose.model('User', userSchema);
