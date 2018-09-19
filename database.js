e0520e5aeb4267802c6692bdb81feccc3fed20bc
carlos martin <cjmar87@gmail.com>
2018-09-19 12:18:20 -0500

    se cambia la db

--- database.js
+++ database.js
@@ -2,8 +2,8 @@
 
 const mongoose = require('mongoose');
 //const URI = 'mongodb://charly_mrtn:9torres9@cluster1-shard-00-00-gluuy.mongodb.net:27017,cluster1-shard-00-01-gluuy.mongodb.net:27017,cluster1-shard-00-02-gluuy.mongodb.net:27017/mean-productos?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin';
-//const URI = 'mongodb+srv://charly_mrtn:9torres9@cluster1-gluuy.mongodb.net/api-productos?retryWrites=true';
-const URI = 'mongodb://pajaro:cable.100@ds163382.mlab.com:63382/heroku_bm5rm326'
+const URI = 'mongodb+srv://charly_mrtn:9torres9@cluster1-gluuy.mongodb.net/api-productos?retryWrites=true';
+//const URI = 'mongodb://pajaro:cable.100@ds163382.mlab.com:63382/heroku_bm5rm326'
 mongoose.connect(URI)
     .then(db => console.log('db is connected'))
     .catch(err => console.error(err));
